4e77ae5ffea04fd1eb0c7ef2e864a76b36649aa2
gandalf0151 <gandalf0151@gmail.com>
2021-05-07 18:42:30 +0530

    Added base set of configurations/setup for competitive programming

---
+++ code-config/make_prob.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname $0)
+search_up ()
+(
+    while [[ $PWD != "/" ]]; do
+        if [[ -e "$1" ]]; then
+            pwd
+            if [[ ! -e "$1/$2" ]]; then
+                break
+            fi
+        fi
+        cd ..
+    done
+)
+TEMPLATE_DIR='.template'
+PARENT_FILE='$PARENT'
+IFS=$'\n'
+TEMPLATE_DIRS=($(search_up "$TEMPLATE_DIR" "$PARENT_FILE" | tac))
+unset IFS
+TEMPLATE_DIRS=(${TEMPLATE_DIRS[@]/%/\/"$TEMPLATE_DIR"})
+
+if hash rename.ul 2>/dev/null; then
+    RENAME=rename.ul
+else
+    RENAME=rename
+fi
+
+for filepath in "$@"; do
+    PROBLEM_NAME=$(basename "$filepath")
+
+    if [[ -e "$filepath" ]]; then
+        echo "$filepath already exists. Remove it and retry."
+        continue
+    fi
+
+    # Copy files in
+    mkdir -p "$filepath"
+    for CURRENT_TEMPLATE_DIR in "${TEMPLATE_DIRS[@]}"; do
+        cp -r -T "$CURRENT_TEMPLATE_DIR" "$filepath/"
+    done
+    rm -f "$filepath/$PARENT_FILE"
+    cat ~/programming/template.cpp > ./$filepath/"$PROBLEM_NAME".cpp; echo "created '$PROBLEM_NAME'.cpp file"; cp ~/programming/Makefile ./$filepath/; sed -i '1s/^/PROBLEM_NAME := '$PROBLEM_NAME'\n/' ./$filepath/Makefile;
+
+    pushd $filepath > /dev/null
+    if [[ -e "setup" ]]; then
+        ./setup
+    fi
+    popd > /dev/null
+done
