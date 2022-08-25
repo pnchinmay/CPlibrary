6636364c0630bf5b3def929c3ea49a6a7131c371
pnchinmay <pnchinmay9@gmail.com>
2022-08-25 23:17:28 +0530

    feat: modified template file
    - simplified for loop
    - add reverse for loop
    - add debugger statement
    - add macros for vector, pair, first, second, maxheap, unordered_set and unordered_map
    - add big infinity and big mod values

--- code-config/$PROBLEM_NAME.cpp
+++ code-config/$PROBLEM_NAME.cpp
@@ -7,15 +7,31 @@ using namespace std;
 
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
-#define forn(i,n) for(ll (i)=0; (i)<(n); (i)++)
-#define forx(i,x,n) for(ll (i)=(x); (i)<(n); (i)++)
+
+typedef long long ll;
+
+#define llvec vector<ll>
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define vi vector<int>
+#define fe first
+#define se second
+#define maxheap priority_queue
+#define uset unordered_set
+#define umap unordered_map
+
+#define fr(i,x,n) for(ll (i)=(x); (i)<(n); (i)++)
+#define revfr(i, x, n) for(big i = x - 1; i >= n; i--)
+
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+
+#define debug(text) if(do_debug) {cout << text << endl;}
 
 const ll N=1e4+5;
+const big mod = 1000000007;
+const big infinity = 1000000000000000000;
+bool do_debug = false;
 
 int solve()
 {
